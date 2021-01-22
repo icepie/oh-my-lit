d5497a23c33dd3a1bafc0d8b75a329ce081c7279
icepie <icepie.dev@gmail.com>
2021-01-22 22:23:10 +0800

    header

--- service/jw_login.go
+++ service/jw_login.go
@@ -132,11 +132,25 @@ func SendLogin(username string, password string) ([]*http.Cookie, error) {
 
 	client := &http.Client{}
 	r, _ := http.NewRequest(http.MethodPost, LoginURL, strings.NewReader(data.Encode()))
-	//r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Host", "jw.sec.lit.edu.cn")
+	r.Header.Add("Proxy-Connection", "keep-alive")
+	// r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	// r.Header.Add("Origin", "http://jw.sec.lit.edu.cn")
+	// r.Header.Add("Upgrade-Insecure-Requests", "1")
+	// r.Header.Add("DNT", "1")
+	// r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	// r.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36")
+	// r.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	// r.Header.Add("Referer", LoginURL)
+	// r.Header.Add("Accept-Encoding", "gzip, deflate")
+	// r.Header.Add("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
+
 	jar, _ := cookiejar.New(nil)
 	jar.SetCookies(r.URL, cookie)
 
 	resp, _ := client.Do(r)
+
+	fmt.Println(resp.Cookies())
 
 	fmt.Println(resp.Request)
 
@@ -154,10 +168,13 @@ func SendLogin(username string, password string) ([]*http.Cookie, error) {
 	// })
 
 	r, _ = http.NewRequest(http.MethodGet, MenuURL, nil)
+
 	jar, _ = cookiejar.New(nil)
 	jar.SetCookies(r.URL, cookie)
 
 	resp, _ = client.Do(r)
+
+	fmt.Println(resp.Cookies())
 
 	// 青果不是utf-8 所以要转换一下
 	utf8Body, err := iconv.NewReader(resp.Body, "gb2312", "utf-8")
