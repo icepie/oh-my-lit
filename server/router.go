5c845b65ead30ba5509cbd0116b3311cf9a4db8e
icepie <icepie.dev@gmail.com>
2021-03-14 11:35:23 +0800

    add v0 and v1 api5

--- server/router.go
+++ server/router.go
@@ -10,24 +10,14 @@ import (
 func ApiRouter() *gin.Engine {
 	r := gin.Default()
 
-	// v0 路由: 学生帐号行为
+	// v0 路由: 学生帐号服务
 	v0 := r.Group("api/v0")
 	{
 		// 测试连接
 		v0.GET("ping", api.PingPong)
-		jw := v0.Group("jw")
-		{
-			// 测试教务服务连接性
-			jw.GET("status", api.JWGetStatus)
-			// 获取学生基础个人信息
-			jw.POST("profile", api.JWGetBaseInfo)
-			// 获取学生成绩
-			jw.POST("score", api.JWGetScore)
-		}
-
 	}
 
-	// v1 路由: 管理员帐号行为
+	// v1 路由: 管理员帐号服务
 	v1 := r.Group("api/v1")
 	{
 		// 测试连接
@@ -44,11 +34,11 @@ func ApiRouter() *gin.Engine {
 
 	}
 
-	// v1 路由: 喜鹊儿接口 (unstable)
-	v3 := r.Group("api/v3")
+	// v2 路由: 喜鹊儿接口 (unstable)
+	v2 := r.Group("api/v3")
 	{
 		// 测试连接
-		v3.GET("ping", api.PingPong)
+		v2.GET("ping", api.PingPong)
 	}
 
 	return r
